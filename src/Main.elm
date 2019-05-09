5c5ecafba5eae173f104199696a3c01e6c940130
quilicicf <cquilici@restlet.com>
2019-05-09 18:56:11 +0200

    :up: Store app config to local storage

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (main)
+port module Main exposing (main)
 
 import Browser exposing (application, UrlRequest, Document)
 import Browser.Navigation exposing (Key, load, pushUrl)
@@ -7,14 +7,24 @@ import Debug exposing (log)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 import Url exposing (Url)
 
+import Base64 exposing (..)
+
 import Json.Decode exposing (Value)
+import Json.Encode as Encode
+
+port setStorage : AppConfig -> Cmd msg
 
 type Msg
   = LinkClicked UrlRequest
   | UrlChanged Url
+  | NewTeamName String
+  | NewSheetId String
+  | NewApiKey String
+  | CreateAppConfig
 
 type alias AppConfig =
   { teamName: String
@@ -26,6 +36,7 @@ type Page
   = WelcomePage
   | AppConfigPage
   | AppKeyPage
+  | StatsPage
   | NotFoundPage
 
 type alias Model =
@@ -35,11 +46,28 @@ type alias Model =
   , navigationKey: Key
   }
 
+
+jsonifyAppConfig : AppConfig -> Value
+jsonifyAppConfig appConfig =
+  Encode.object
+      [ ("teamName", Encode.string appConfig.teamName)
+      , ("sheetId", Encode.string appConfig.sheetId)
+      , ("apiKey", Encode.string appConfig.apiKey)
+      ]
+
+encodeAppConfig : AppConfig -> String
+encodeAppConfig appConfig = appConfig
+  |> jsonifyAppConfig
+  |> Encode.encode 0
+  |> Base64.encode
+
 welcomeScreen : Model -> Html Msg
 welcomeScreen _ =
   div []
     [ text "Choose your side"
+    , br [] []
     , a [ href "/appConfig" ] [ text "I'm the alliance's GOD" ]
+    , br [] []
     , a [ href "/appKey" ] [ text "I'm but a peon" ]
     ]
 
@@ -53,6 +81,7 @@ appConfigForm appConfig =
         , input
             [ type_ "text"
             , value appConfig.teamName
+            , onInput NewTeamName
             ]
             []
         ]
@@ -63,6 +92,7 @@ appConfigForm appConfig =
         , input
             [ type_ "text"
             , value appConfig.sheetId
+            , onInput NewSheetId
             ]
             []
         ]
@@ -73,13 +103,14 @@ appConfigForm appConfig =
         , input
             [ type_ "text"
             , value appConfig.apiKey
+            , onInput NewApiKey
             ]
             []
         ]
     , br [] []
     , div []
         [ button
-            [ type_ "button" ]
+            [ type_ "button", onClick CreateAppConfig ]
             [ text "Create" ]
         ]
     ]
@@ -121,10 +152,18 @@ findPage url =
     "/" -> WelcomePage
     "/appConfig" -> AppConfigPage
     "/appKey" -> AppKeyPage
+    "/stats" -> StatsPage
     _ -> NotFoundPage
 
+updateWithStorage : Msg -> Model -> ( Model, Cmd Msg )
+updateWithStorage msg model =
+  let
+    ( ({ appConfig } as newModel), cmds ) = update msg model
+  in
+    ( newModel, Cmd.batch [ setStorage appConfig, cmds ] )
+
 update : Msg -> Model -> (Model, Cmd Msg)
-update msg model =
+update msg ({ appConfig } as model) =
   case msg of
     LinkClicked urlRequest ->
       case urlRequest of
@@ -139,6 +178,35 @@ update msg model =
       in
         log url.path
         ( { model | currentPage = newPage }, Cmd.none )
+
+    NewTeamName newTeamName ->
+      let
+        newAppConfig : AppConfig
+        newAppConfig = { appConfig | teamName = newTeamName }
+      in
+        ( { model | appConfig = newAppConfig }, Cmd.none )
+
+    NewSheetId newSheetId ->
+      let
+        newAppConfig : AppConfig
+        newAppConfig = { appConfig | sheetId = newSheetId }
+      in
+        ( { model | appConfig = newAppConfig }, Cmd.none )
+
+    NewApiKey newApiKey ->
+      let
+        newAppConfig : AppConfig
+        newAppConfig = { appConfig | apiKey = newApiKey }
+      in
+        ( { model | appConfig = newAppConfig }, Cmd.none )
+
+    CreateAppConfig ->
+      let
+        toto = log "Toto" appConfig
+        newAppKey : String
+        newAppKey = encodeAppConfig appConfig
+      in
+        ( { model | appKey = newAppKey }, pushUrl model.navigationKey "/stats" )
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
@@ -178,6 +246,14 @@ view model =
         ]
       })
 
+    StatsPage -> (
+      { title = "EP stats"
+      , body = [
+          div []
+            [ h1 [] [ text "Stats should go here" ] ]
+        ]
+      })
+
     NotFoundPage -> (
       { title = "EP stats"
       , body = [
@@ -192,7 +268,7 @@ main : Program Value Model Msg
 main = application
   { init = init
   , view = view
-  , update = update
+  , update = updateWithStorage
   , subscriptions = subscriptions
   , onUrlChange = UrlChanged
   , onUrlRequest = LinkClicked

6c21774e8c8c15ca413dac27652eba558c758714
quilicicf <cquilici@restlet.com>
2019-07-01 21:00:51 +0200

    :bug: Redirect all paths to index.html

--- build/constants.js
+++ build/constants.js
@@ -4,19 +4,22 @@ const DIR_PATH = resolvePath(__dirname, '..');
 const SRC_PATH = resolvePath(DIR_PATH, 'src');
 const DIST_PATH = resolvePath(DIR_PATH, 'dist');
 const APP_HTML_NAME = 'index.html';
+const DEPLOYMENT_NAME = 'netlify.toml';
 
 module.exports = {
   DIR_PATH,
   DIST_PATH,
   APP_HTML_NAME,
-  APP_HTML_SOURCE_PATH: resolvePath(DIR_PATH, APP_HTML_NAME),
 
   SRC_PATH,
+  APP_HTML_SOURCE_PATH: resolvePath(DIR_PATH, APP_HTML_NAME),
   APP_ENTRY_POINT: resolvePath(SRC_PATH, 'Main.elm'),
   STYLE_ENTRY_POINT: resolvePath(SRC_PATH, 'app.scss'),
+  DEPLOYMENT_SOURCE_PATH: resolvePath(DIR_PATH, DEPLOYMENT_NAME),
 
   APP_HTML_OUTPUT_PATH: resolvePath(DIST_PATH, APP_HTML_NAME),
   APP_OUTPUT_PATH: resolvePath(DIST_PATH, 'app.js'),
   STYLE_OUTPUT_PATH: resolvePath(DIST_PATH, 'app.css'),
   STYLE_SOURCE_MAP_PATH: resolvePath(DIST_PATH, 'app.css.map'),
+  DEPLOYMENT_OUTPUT_PATH: resolvePath(DIST_PATH, DEPLOYMENT_NAME),
 };
--- build/prepareBuild.js
+++ build/prepareBuild.js
@@ -2,12 +2,19 @@ const { existsSync, mkdirSync, copyFileSync } = require('fs');
 
 const writeFile = require('./writeFile');
 const {
-  STYLE_OUTPUT_PATH, STYLE_SOURCE_MAP_PATH, DIST_PATH, APP_HTML_SOURCE_PATH, APP_HTML_OUTPUT_PATH,
+  DIST_PATH,
+  STYLE_OUTPUT_PATH,
+  STYLE_SOURCE_MAP_PATH,
+  APP_HTML_SOURCE_PATH,
+  APP_HTML_OUTPUT_PATH,
+  DEPLOYMENT_SOURCE_PATH,
+  DEPLOYMENT_OUTPUT_PATH,
 } = require('./constants');
 
 module.exports = async () => {
   if (!existsSync(DIST_PATH)) { mkdirSync(DIST_PATH); }
   copyFileSync(APP_HTML_SOURCE_PATH, APP_HTML_OUTPUT_PATH);
+  copyFileSync(DEPLOYMENT_SOURCE_PATH, DEPLOYMENT_OUTPUT_PATH);
 
   if (!existsSync(STYLE_OUTPUT_PATH)) { await writeFile(STYLE_OUTPUT_PATH, ''); }
   if (!existsSync(STYLE_SOURCE_MAP_PATH)) { await writeFile(STYLE_SOURCE_MAP_PATH, ''); }

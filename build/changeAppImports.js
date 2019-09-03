f809a6bea8143a4539f8da5f6878be2e524e6e0e
quilicicf <cquilici@restlet.com>
2019-09-03 14:14:38 +0200

    :bug: Fix HTML replacers

--- build/changeAppImports.js
+++ build/changeAppImports.js
@@ -4,8 +4,8 @@ function changeImports () {
   return through(
     function write (chunk) {
       this.queue(chunk.toString()
-        .replace(/script src="app\.js"/, 'script src="app.min.js"'))
-        .replace(/rel="stylesheet" href="app\.css"/, 'rel="stylesheet" href="app.min.css"');
+        .replace(/script src="app\.js"/, 'script src="app.min.js"')
+        .replace(/rel="stylesheet" href="app\.css"/, 'rel="stylesheet" href="app.min.css"'));
     },
     function end () {
       this.queue(null);
